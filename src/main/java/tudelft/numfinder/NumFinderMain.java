06cdc98db7bc6b15c75328145c5f90204f354d04
sule <sule.yavuz.905@gmail.com>
2025-04-29 19:13:18 +0300

    fix: corrected bug in NumFinder by replacing else-if with separate if test: added one working and one crashing test case in NumFinderMain

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -5,13 +5,20 @@ public class NumFinderMain {
     public static void main (String[] args) {
         NumFinder nf = new NumFinder();
 
-        // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+        //this works
+        nf.find(new int[]{4, 25, 7, 9});
+        System.out.println("Normal input: ");
+        System.out.println("Max: " + nf.getLargest());
+        System.out.println("Min: " + nf.getSmallest());
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
-
-        System.out.println(nf.getLargest());
-        System.out.println(nf.getSmallest());
+        try {
+            nf.find(null);
+            System.out.println("Null input: ");
+            System.out.println("Max: " + nf.getLargest());
+            System.out.println("Min: " + nf.getSmallest());
+        } catch (Exception e) {
+            System.out.println("Null input caused error: " + e);
+        }
     }
 }
