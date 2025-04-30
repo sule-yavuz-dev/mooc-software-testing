a6c2cfab2d2b2df711a6dcb7521260f3a94cc9e2
sule <sule.yavuz.905@gmail.com>
2025-04-30 12:16:29 +0300

    Fix mirrorEnds implementation and add passing unit tests

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -3,19 +3,16 @@ package tudelft.mirror;
 public class Mirror {
 
     public String mirrorEnds(String string) {
-        String mirror = "";
+        StringBuilder mirror = new StringBuilder();
 
-        int begin = 0;
-        int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
-            if (string.charAt(begin) == string.charAt(end)) {
-                mirror += String.valueOf(string.charAt(end));
-            }
-            else {
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == string.charAt(string.length() - 1 - i)) {
+                mirror.append(string.charAt(i));
+            } else {
                 break;
             }
         }
 
-        return begin == end ? string : mirror;
+        return mirror.toString();
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,30 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MirrorTest {
+
+    @Test
+    public void testMirrorEnds_basicCase() {
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("abXYZba");
+        assertEquals("ab", result);
+    }
+
+
+    @Test
+    public void testMirrorEnds_noMatch() {
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("abcdefg");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testMirrorEnds_emptyString() {
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("");
+        assertEquals("", result);
+    }
+
 }
